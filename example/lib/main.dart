5fda784d9a595dc971a990bc074a54d8fbcc3b67
laoguanyao <806103474@qq.com>
2024-12-04 10:15:20 +0800

    修改对应字体类型，保持3.7和3.22一致
    Signed-off-by: laoguanyao <806103474@qq.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -90,7 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineLarge,
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
             InkWell(
                 child: Icon(
